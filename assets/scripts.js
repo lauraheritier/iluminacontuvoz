ebdaee6e5505180efc5e814c2becd635a0f8e400
Laura Heritier <laura.heritier@gmail.com>
2024-01-30 01:20:21 -0300

    Add files via upload

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,12 +1,5 @@
 $(document).ready(function() {    
     AOS.init();
-    $(".nav-item").on("click", function() {
-        $(this).addClass("active");
-        $(this).siblings(".nav-item").removeClass("active");
-    });
-    $(".small-menu-container .nav-link").on("click", function() {
-        $("input").prop("checked", false);
-    });
     $(window).scroll(function() {
         if ($(this).scrollTop()) {
             $('#toTop').fadeIn();
